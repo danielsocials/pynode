726d8037e2ac7b2b16800970633032ba64474e60
daniel <daniel.socials@gmail.com>
2018-08-20 13:05:41 +0800

    clean up

--- pynode.py
+++ pynode.py
@@ -22,37 +22,16 @@ def run():
     if config.enable_server:
         server = Server(ip="127.0.0.1", port=9999, node_id=node_id, db=db)
         server.loop()
-    #exit(0)
-
-
-    #asyncore.loop()
-    #server.ip = server.get_ip(ifname='enp0s31f6')
-    #asyncore.loop()
-    #async_thread = threading.Thread(target=asyncore.loop, name="async_loop")
-    #async_thread.start()
-
 
     for peer in config.peers:
         ip, port = peer.split(':')
         client =  Client(ip, int(port), node_id, db)
         client.send_handshake_msg()
-        #client.loop()
-        #client.send_time_msg()
-        #time.sleep(3)
-        #client.send_sync_request_message()
-        #msg_thread = Network_thread(client)
-        #msg_thread.run()
     node_thread = threading.Thread(target=client.loop, name="node loop")
     node_thread.start()
     if config.enable_rpcserver:
         msg_thread = threading.Thread(target=rpc_server(8888, client), name="Rpc server")
         msg_thread.setDaemon(True)
         msg_thread.start()
-    #client.loop()
-
-    #msg_thread = threading.Thread(target=msg.loop, name="async_loop")
-    #async_thread.start()
-
-    #client.loop()
 
 run()
